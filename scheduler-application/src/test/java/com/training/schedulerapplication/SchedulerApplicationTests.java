445f38aa352a17fea22010e00f6bba4611e5ea3f
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-02-08 16:43:35 +0200

    Initial commit

---
+++ scheduler-application/src/test/java/com/training/schedulerapplication/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.schedulerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
