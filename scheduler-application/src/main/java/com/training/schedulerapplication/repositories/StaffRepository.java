47a67846fe65aaf13df4d7656d871a0e475a623c
Thomas Stevens <thomas@bbd.co.za>
2023-02-16 08:17:35 +0200

    H2 database created, but cant query it

--- scheduler-application/src/main/java/com/training/schedulerapplication/repositories/StaffRepository.java
+++ scheduler-application/src/main/java/com/training/schedulerapplication/repositories/StaffRepository.java
@@ -2,6 +2,15 @@ package com.training.schedulerapplication.repositories;
 
 import com.training.schedulerapplication.models.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface StaffRepository extends JpaRepository<Staff, Long> {
+    @Query(value = "SELECT id, firstName, lastName, role FROM staff", nativeQuery = true)
+    public List<Staff> findAll();
+
+    @Query(value = "SELECT id, firstName, lastName, role FROM staff WHERE id = ?1", nativeQuery = true)
+    public Optional<Staff> findById(Long id);
 }
